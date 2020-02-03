679f1800005c7b6924c046a222874e1d86ebf545
Polixenia Correia <polixeniacorreia@gmail.com>
2020-02-03 12:50:40 -0300

    Demo - inserir dados

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,39 +1,73 @@
 package aplication;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
+
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
-		Statement st = null;
-		ResultSet rs = null;
-		
+		PreparedStatement st = null;
+
 		try {
 			conn = DB.getConnection();
-			st = conn.createStatement();
-			rs = st.executeQuery("select * from department");
-			
-			while (rs.next()) {
-				System.out.println(rs.getInt("Id") + "," + rs.getString("Name"));
-				
+
+			/*
+
+			st = conn.prepareStatement(
+					"INSERT INTO seller "
+							+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
+							+ "VALUES "
+							+ "(?, ?, ?, ?, ?)",
+							Statement.RETURN_GENERATED_KEYS);
+
+			st.setString(1, "Poli");
+			st.setString(2, "poli@gmail.com");
+			st.setDate(3, new java.sql.Date(sdf.parse("03/02/2020").getTime()));
+			st.setDouble(4, 5000.00);
+			st.setInt(5, 4);
+
+			 */
+
+			st = conn.prepareStatement(
+					"insert into department (Name) values ('D1'),('D2')",
+					Statement.RETURN_GENERATED_KEYS);
+
+
+			int rowsAffected = st.executeUpdate();
+
+			if(rowsAffected > 0) {
+				ResultSet rs = st.getGeneratedKeys();
+
+				while(rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Pronto! ID = " +id);
+				}	
+
+			}else {
+				System.out.println("Nenhuma linha foi alterada.");
 			}
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 		finally {
-			DB.closeResultSet(rs);
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-		
-		
+
+
+
+
 	}
 }
--- src/db/DB.java
+++ src/db/DB.java
@@ -58,27 +58,29 @@ public class DB {
 	}
 	
 	public static void closeStatement(Statement st) {
-		if(st != null) {
+		
+		if (st != null) {
 			try {
 				st.close();
-			} catch (SQLException e) {
+			} 
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
-	
+
 	public static void closeResultSet(ResultSet rs) {
-		if(rs != null) {
+		
+		if (rs != null) {
 			try {
 				rs.close();
-			} catch (SQLException e) {
+			} 
+			catch (SQLException e) {
 				throw new DbException(e.getMessage());
 			}
 		}
 	}
 	
-	
-	
-	
+		
 	
 }
