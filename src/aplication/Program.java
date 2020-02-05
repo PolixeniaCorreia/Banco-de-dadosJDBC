96c56a496e4a26398f102c8c37116b39537aa0ae
Polixenia Correia <polixeniacorreia@gmail.com>
2020-02-05 16:38:26 -0300

    Demo - atualizar dados

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -15,57 +15,33 @@ public class Program {
 
 	public static void main(String[] args) {
 
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		Connection conn = null;
 		PreparedStatement st = null;
-
+		
 		try {
 			conn = DB.getConnection();
-
-			/*
-
+			
 			st = conn.prepareStatement(
-					"INSERT INTO seller "
-							+ "(Name, Email, BirthDate, BaseSalary, DepartmentId) "
-							+ "VALUES "
-							+ "(?, ?, ?, ?, ?)",
-							Statement.RETURN_GENERATED_KEYS);
+					"UPDATE seller "
+					+ "SET BaseSalary = BaseSalary + ? "
+					+ "WHERE "
+					+ "(DepartmentId = ?)");
 
-			st.setString(1, "Poli");
-			st.setString(2, "poli@gmail.com");
-			st.setDate(3, new java.sql.Date(sdf.parse("03/02/2020").getTime()));
-			st.setDouble(4, 5000.00);
-			st.setInt(5, 4);
-
-			 */
-
-			st = conn.prepareStatement(
-					"insert into department (Name) values ('D1'),('D2')",
-					Statement.RETURN_GENERATED_KEYS);
-
-
+			st.setDouble(1, 200.0);
+			st.setInt(2, 2);
+			
 			int rowsAffected = st.executeUpdate();
-
-			if(rowsAffected > 0) {
-				ResultSet rs = st.getGeneratedKeys();
-
-				while(rs.next()) {
-					int id = rs.getInt(1);
-					System.out.println("Pronto! ID = " +id);
-				}	
-
-			}else {
-				System.out.println("Nenhuma linha foi alterada.");
-			}
-
-		} catch (SQLException e) {
+			System.out.println("Pronto! Linhas Afedatas: " + rowsAffected);
+			
+		}
+		catch (SQLException e) {
 			e.printStackTrace();
 		}
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-
+		
 
 
 
