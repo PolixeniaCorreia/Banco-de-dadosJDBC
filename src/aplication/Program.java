d6a5366b37f058688532355384a3638843d4b68b
Polixenia Correia <polixeniacorreia@gmail.com>
2020-02-05 16:51:19 -0300

    Demo - deletar dados

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -9,6 +9,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 
 import db.DB;
+import db.DbIntegrityException;
 
 
 public class Program {
@@ -22,28 +23,23 @@ public class Program {
 			conn = DB.getConnection();
 			
 			st = conn.prepareStatement(
-					"UPDATE seller "
-					+ "SET BaseSalary = BaseSalary + ? "
+					"DELETE FROM department "
 					+ "WHERE "
-					+ "(DepartmentId = ?)");
+					+ "Id = ?");
 
-			st.setDouble(1, 200.0);
-			st.setInt(2, 2);
+			st.setInt(1, 2);
 			
 			int rowsAffected = st.executeUpdate();
 			System.out.println("Pronto! Linhas Afedatas: " + rowsAffected);
 			
 		}
 		catch (SQLException e) {
-			e.printStackTrace();
+			throw new DbIntegrityException(e.getMessage());
 		}
 		finally {
 			DB.closeStatement(st);
 			DB.closeConnection();
 		}
-		
-
-
-
+	
 	}
 }
